d956201cc3947a596fe9d9e97b27530129f3b404
ganipadela <ganipadela@gmail.com>
2018-02-17 01:07:47 +0530

    Zip code validator using loopback validate

--- common/models/booking.js
+++ common/models/booking.js
@@ -1,5 +1,9 @@
 'use strict';
 
-module.exports = function(Booking) {
-
+module.exports = function (Booking) {
+  // Function to validate zipcode
+  function zipValidator(err) {
+    if (this.zip && this.zip.toString().length > 5) err();
+  }
+  Booking.validate('zip', zipValidator, { message: 'Invalid zip' });
 };
