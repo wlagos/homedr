708e3178003d94d4e780c236aea000b5dc704545
ganipadela <ganipadela@gmail.com>
2018-02-20 00:17:51 +0530

    User check if disabled feature

---
+++ server/boot/check-user-disabled.js
@@ -0,0 +1,31 @@
+const createError = (status, message) => {
+  let errorObj = new Error(message);
+  errorObj.statusCode = status;
+  return errorObj;
+}
+
+module.exports = async function (app, cb) {
+  app.remotes().phases
+    .addBefore('auth', 'check-user-disabled')
+    .use(async function (ctx, next) {
+      const req = ctx.req;
+      if (!req.accessToken) {
+        return next();
+      }
+      const AppUser = app.models.AppUser;
+      let userId = req.accessToken.userId;
+      try {
+        let userInstance = await AppUser.findById(userId);
+        if (!userInstance) {
+          return next(createError(404, 'User not found!'));
+        }
+        if (userInstance.isDisabled) {
+          return next(createError(403, 'Your account has been disabled!'));
+        }
+        next();
+      } catch (error) {
+        return next(error);
+      }
+    });
+  cb();
+}
